522f6c16df4ba536bd15b7f65c689cad66a18659
anastasiia sineok <anastasiia.synieok@oa.edu.ua>
2025-03-23 12:32:41 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TodoList from "./TodoList";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Todo List App</h1>
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
